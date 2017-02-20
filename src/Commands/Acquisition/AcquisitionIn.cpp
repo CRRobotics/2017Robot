4ee37068e2630b1dfc4724ad41511cdae882144b
Adam Newhouse <adnewhouse@gmail.com>
2017-02-20 01:06:19 -0500

    Lol. Forgot to remove this.

--- src/Commands/Acquisition/AcquisitionIn.cpp
+++ src/Commands/Acquisition/AcquisitionIn.cpp
@@ -3,7 +3,6 @@
 AcquisitionIn::AcquisitionIn() {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
-	Requires(Robot::storage.get());
 }
 
 // Called just before this Command runs the first time
